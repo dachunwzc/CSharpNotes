b73424f018abcf7c77ebf8d3214001352ac5224f
dachunwzc <dachunwzc@outlook.com>
2023-05-21 20:53:12 +0800

    ComboxNote-1-初次使用

--- WPFNotes/DataTemplate/ComboxNote.xaml.cs
+++ WPFNotes/DataTemplate/ComboxNote.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataTemplate.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace DataTemplate
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new StudentViewModel();
         }
     }
 }
---
+++ WPFNotes/DataTemplate/ViewModel/StudentViewModel.cs
@@ -0,0 +1,28 @@
+﻿using DataTemplate.Model;
+using System.Collections.ObjectModel;
+
+namespace DataTemplate.ViewModel
+{
+    internal class StudentViewModel
+    {
+        public ObservableCollection<StudentModel> StudentModelCollection { get; set; } = new ObservableCollection<StudentModel>();
+        public StudentModel SelectedStudentModel { get; set; }
+
+        public StudentViewModel()
+        {
+            StudentModel studentModel1 = new StudentModel(1, "宋一");
+            StudentModel studentModel2 = new StudentModel(2, "赵二");
+            StudentModel studentModel3 = new StudentModel(3, "张三");
+            StudentModel studentModel4 = new StudentModel(4, "李四");
+            StudentModel studentModel5 = new StudentModel(5, "王老五");
+
+            StudentModelCollection.Add(studentModel1);
+            StudentModelCollection.Add(studentModel2);
+            StudentModelCollection.Add(studentModel3);
+            StudentModelCollection.Add(studentModel4);
+            StudentModelCollection.Add(studentModel5);
+
+            SelectedStudentModel = StudentModelCollection[0];
+        }
+    }
+}
