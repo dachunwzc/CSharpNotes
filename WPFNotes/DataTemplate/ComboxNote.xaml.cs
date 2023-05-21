f4e1ebbae054efe11204e05d465b69e067d71b63
dachunwzc <dachunwzc@outlook.com>
2023-05-21 21:33:54 +0800

    ListViewNote-1-DataTemplate初始化

--- WPFNotes/DataTemplate/ComboxNote.xaml.cs
+++ WPFNotes/DataTemplate/ComboxNote.xaml.cs
@@ -1,30 +1,17 @@
 ﻿using DataTemplate.ViewModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DataTemplate
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ComboxNote : Window
     {
-        public MainWindow()
+        public ComboxNote()
         {
             InitializeComponent();
-            this.DataContext = new StudentViewModel();
+            DataContext = new StudentViewModel();
         }
     }
 }
