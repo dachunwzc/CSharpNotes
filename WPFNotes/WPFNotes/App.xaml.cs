f78014fd405f46aafcef5362948e470c99d2bc60
dachunwzc <dachunwzc@outlook.com>
2023-05-21 19:17:42 +0800

    修正项目结构

--- WPFNotes/WPFNotes/App.xaml.cs
+++ WPFNotes/WPFNotes/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Initialization
+namespace WPFNotes
 {
     /// <summary>
     /// App.xaml 的交互逻辑
--- WPFNotes/WPFNotes/Properties/Settings.Designer.cs
+++ WPFNotes/WPFNotes/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Initialization.Properties
+namespace WPFNotes.Properties
 {
 
 
